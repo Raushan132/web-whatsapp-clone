a2281ed341c73851e75ae54bc7afa8bc6603a25f
Raushan132 <75939697+Raushan132@users.noreply.github.com>
2021-01-10 09:39:41 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="style/contact.css">
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/hover.js"></script>
+</head>
+<body>
+<div>
+	<div class="container">
+		<div class="content-box1">
+			<div class="box-img"><img src="image/home.jpg"></div>
+			<div class="box-name">Raushan Kumar</div>
+			<div class="box-mess">last message</div>
+			<div class="box-time">7:32 pm</div>
+			<div class="arrow"><img src="image/down-arrow.png"></div>
+		</div>
+
+         <!-- demo detail test  you delete useing php coding down here-->
+
+          <div class="content-box1">
+			<div class="box-img"><img src="image/home.jpg"></div>
+			<div class="box-name">Raushan Kumar</div>
+			<div class="box-mess">last message</div>
+			<div class="box-time">7:32 pm</div>
+			<div class="arrow"><img src="image/down-arrow.png"></div>
+		</div><div class="content-box1">
+			<div class="box-img"><img src="image/home.jpg"></div>
+			<div class="box-name">Raushan Kumar</div>
+			<div class="box-mess">last message</div>
+			<div class="box-time">7:32 pm</div>
+			<div class="arrow"><img src="image/down-arrow.png"></div>
+		</div><div class="content-box1">
+			<div class="box-img"><img src="image/home.jpg"></div>
+			<div class="box-name">Raushan Kumar</div>
+			<div class="box-mess">last message</div>
+			<div class="box-time">7:32 pm</div>
+			<div class="arrow"><img src="image/down-arrow.png"></div>
+		</div><div class="content-box1">
+			<div class="box-img"><img src="image/home.jpg"></div>
+			<div class="box-name">Raushan Kumar</div>
+			<div class="box-mess">last message</div>
+			<div class="box-time">7:32 pm</div>
+			<div class="arrow"><img src="image/down-arrow.png"></div>
+		</div><div class="content-box1">
+			<div class="box-img"><img src="image/home.jpg"></div>
+			<div class="box-name">Raushan Kumar</div>
+			<div class="box-mess">last message</div>
+			<div class="box-time">7:32 pm</div>
+			<div class="arrow"><img src="image/down-arrow.png"></div>
+		</div><div class="content-box1">
+			<div class="box-img"><img src="image/home.jpg"></div>
+			<div class="box-name">Raushan Kumar</div>
+			<div class="box-mess">last message</div>
+			<div class="box-time">7:32 pm</div>
+			<div class="arrow"><img src="image/down-arrow.png"></div>
+		</div><div class="content-box1">
+			<div class="box-img"><img src="image/home.jpg"></div>
+			<div class="box-name">Raushan Kumar</div>
+			<div class="box-mess">last message</div>
+			<div class="box-time">7:32 pm</div>
+			<div class="arrow"><img src="image/down-arrow.png"></div>
+		</div><div class="content-box1">
+			<div class="box-img"><img src="image/home.jpg"></div>
+			<div class="box-name">Raushan Kumar</div>
+			<div class="box-mess">last message</div>
+			<div class="box-time">7:32 pm</div>
+			<div class="arrow"><img src="image/down-arrow.png"></div>
+		</div><div class="content-box1">
+			<div class="box-img"><img src="image/home.jpg"></div>
+			<div class="box-name">Raushan Kumar</div>
+			<div class="box-mess">last message</div>
+			<div class="box-time">7:32 pm</div>
+			<div class="arrow"><img src="image/down-arrow.png"></div>
+		</div><div class="content-box1">
+			<div class="box-img"><img src="image/home.jpg"></div>
+			<div class="box-name">Raushan Kumar</div>
+			<div class="box-mess">last message</div>
+			<div class="box-time">7:32 pm</div>
+			<div class="arrow"><img src="image/down-arrow.png"></div>
+		</div><div class="content-box1">
+			<div class="box-img"><img src="image/home.jpg"></div>
+			<div class="box-name">Raushan Kumar</div>
+			<div class="box-mess">last message</div>
+			<div class="box-time">7:32 pm</div>
+			<div class="arrow"><img src="image/down-arrow.png"></div>
+		</div><div class="content-box1">
+			<div class="box-img"><img src="image/home.jpg"></div>
+			<div class="box-name">Raushan Kumar</div>
+			<div class="box-mess">last message</div>
+			<div class="box-time">7:32 pm</div>
+			<div class="arrow"><img src="image/down-arrow.png"></div>
+		</div><div class="content-box1">
+			<div class="box-img"><img src="image/home.jpg"></div>
+			<div class="box-name">Raushan Kumar</div>
+			<div class="box-mess">last message</div>
+			<div class="box-time">7:32 pm</div>
+			<div class="arrow"><img src="image/down-arrow.png"></div>
+		</div><div class="content-box1">
+			<div class="box-img"><img src="image/home.jpg"></div>
+			<div class="box-name">Raushan Kumar</div>
+			<div class="box-mess">last message</div>
+			<div class="box-time">7:32 pm</div>
+			<div class="arrow"><img src="image/down-arrow.png"></div>
+		</div>
+
+
+
+	</div>
+</div>
+</body>
+</html>
---
+++ user-head2.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="style/user-header2.css">
+	<!-- Delete below js and style-->
+	<link rel="stylesheet" type="text/css" href="style/menu.css">
+	<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/hover.js"></script>
+    <!-- Above delete style when you mearge -->
+</head>
+<body>
+<div>
+	<!-- Left side header file and name -->
+	<div class="header">
+		<div class="header-img"><img src="image/home.jpg"></div><!-- friend image  -->
+			<span id="friend-name">Raushan kumar</span><!-- friend name -->
+		
+		<div class="header-Simg"><img src="image/search.png">
+
+		</div><!-- search image -->
+             
+            
+		 <div class="menu">
+		<div class="middle"></div>
+	<span class="pop">menu</span></div>
+
+	</div>
+</div>
+</body>
+</html>
